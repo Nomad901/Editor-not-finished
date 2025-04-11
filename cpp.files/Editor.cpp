6a140975cbea4624ff5f3014f96cc2f7da16005d
Tango <maxim.ant07@gmail.com>
2025-04-11 18:53:48 +0300

    Update Editor.cpp

--- cpp.files/Editor.cpp
+++ cpp.files/Editor.cpp
@@ -1,134 +1,61 @@
 #include "Editor.h"
 
 Editor::Editor(App* app, SDL_Renderer* renderer) 
-	:m_forChoose(0), size(0),
-	number_b(1), menuForEditor(0),
-	menu(app,renderer)
 {
+	menu = std::make_unique<MenuForEditor>(app,renderer);
 	SDL_Color white{ 255,255,255,255 };
-	m_factFront.push_back(std::make_unique<FactoryOfFront>(renderer,white,"Click on the button for the information!",16, "E:/Arial.ttf"));
-	m_factEmpty.push_back(std::make_unique<FactoryOfRect>(renderer,"E:/LabelForText.png"));
-	m_factEmpty[0]->setPosition(974, 5);
-	m_factEmpty[0]->setProperties(300, 100);
-	m_factFront[0]->textToRect(m_factEmpty[0]->getRect());
-}
-
-void Editor::render(SDL_Renderer* renderer)
-{
-	if (menuForEditor == 0) {
-		m_factEmpty[0]->render(renderer);
-		m_factFront[0]->render(renderer);
-
-		//std::cout << lastEl.size() << '\n';
-		for (auto& it : lastEl)
-		{
-			if (it.first == "Grass")
-			{
-				m_factGrass[it.second]->render(renderer);
-			}
-			else if (it.first == "Leaf")
-			{
-				m_factLeaf[it.second]->render(renderer);
-			}
-			else if (it.first == "Trees")
-			{
-				m_factTrees[it.second]->render(renderer);
-			}
-		}
-
-		if (m_forChoose == 1) {
-			m_factGrass[m_factGrass.size() - 1]->render(renderer);
-			m_factLeaf[m_factLeaf.size() - 1]->render(renderer);
-			m_factTrees[m_factTrees.size() - 1]->render(renderer);
-			for (size_t i = 0; i < numbers.size(); i++)
-			{
-				m_factFront[numbers[i]]->render(renderer);
-			}
-		}
-		else {
-			if (!m_factGrass.empty() && size == 1)
-			{
-				m_factGrass[m_factGrass.size() - 1]->render(renderer);
-				if (number_b) {
-					lastEl.emplace(std::make_pair<std::string, int>("Grass", static_cast<int>(m_factGrass.size() - 1)));
-					number_b = 0;
-				}
-			}
-			if (!m_factLeaf.empty() && size == 3)
-			{
-				m_factLeaf[m_factLeaf.size() - 1]->render(renderer);
-				if (number_b) {
-					lastEl.emplace(std::make_pair<std::string, int>("Leaf", static_cast<int>(m_factLeaf.size() - 1)));
-					number_b = 0;
-				}
-			}
-			if (!m_factTrees.empty() && size == 2)
-			{
-				m_factTrees[m_factTrees.size() - 1]->render(renderer);
-				if (number_b) {
-					lastEl.emplace(std::make_pair<std::string, int>("Trees", static_cast<int>(m_factTrees.size() - 1)));
-					number_b = 0;
-				}
-			}
-
-		}
-		auto r = rgbColors.find('r');
-		auto g = rgbColors.find('g');
-		auto b = rgbColors.find('b');
-		auto a = rgbColors.find('a');
-		SDL_SetRenderDrawColor(renderer, r->second, g->second, b->second, a->second);
-	}
-	else {
-		menu.handleRender(renderer);
-	}
+	ptrToImpl->m_factFront.push_back(std::make_unique<FactoryOfFront>(renderer,white,"Click on the button for the information!",16, "E:/Arial.ttf"));
+	ptrToImpl->m_factEmpty.push_back(std::make_unique<FactoryOfRect>(renderer,"E:/LabelForText.png"));
+	ptrToImpl->m_factEmpty[0]->setPosition(974, 5);
+	ptrToImpl->m_factEmpty[0]->setProperties(300, 100);
+	ptrToImpl->m_factFront[0]->textToRect(ptrToImpl->m_factEmpty[0]->getRect());
 }
 
 
 void Editor::freezeBlock(SDL_Renderer* renderer, App* app)
 {
-
 	int posX = app->mouseX();
 	int posY = app->mouseY();
 
-	if (size == 1)
+	if (ptrToImpl->size == 1)
 	{
-		m_factGrass[m_factGrass.size() - 1]->setPosition(posX, posY);
-		size = 0;
+		ptrToImpl->m_factGrass[ptrToImpl->m_factGrass.size() - 1]->setPosition(posX, posY);
+		ptrToImpl->size = 0;
 	}
-	else if (size == 3)
+	else if (ptrToImpl->size == 2)
 	{
-		m_factLeaf[m_factLeaf.size() - 1]->setPosition(posX, posY);
-		size = 0;
+		ptrToImpl->m_factTrees[ptrToImpl->m_factTrees.size() - 1]->setPosition(posX, posY);
+		ptrToImpl->size = 0;
 	}
-	else if (size == 2)
+	else if (ptrToImpl->size == 3)
 	{
-		m_factTrees[m_factTrees.size() - 1]->setPosition(posX, posY);
-		size = 0;
+		ptrToImpl->m_factLeaf[ptrToImpl->m_factLeaf.size() - 1]->setPosition(posX, posY);
+		ptrToImpl->size = 0;
 	}
 }
 
 void Editor::chooseBlock(SDL_Renderer* renderer)
 {
-	m_factGrass.push_back(std::make_unique<FactoryOfRect>(renderer, "E:/grass.png"));
-	m_factLeaf.push_back(std::make_unique<FactoryOfRect>(renderer, "E:/leaf.png"));
-	m_factTrees.push_back(std::make_unique<FactoryOfRect>(renderer, "E:/wood.png"));
+	ptrToImpl->m_factGrass.push_back(std::make_unique<FactoryOfRect>(renderer, "E:/grass.png"));
+	ptrToImpl->m_factTrees.push_back(std::make_unique<FactoryOfRect>(renderer, "E:/wood.png"));
+	ptrToImpl->m_factLeaf.push_back(std::make_unique<FactoryOfRect>(renderer, "E:/leaf.png"));
 
-	m_factGrass[m_factGrass.size() - 1]->setProperties(100, 100);
-	m_factLeaf[m_factLeaf.size() - 1]->setProperties(100, 100);
-    m_factTrees[m_factTrees.size() - 1]->setProperties(100, 100);
+	ptrToImpl->m_factGrass[ptrToImpl->m_factGrass.size() - 1]->setProperties(100, 100);
+	ptrToImpl->m_factTrees[ptrToImpl->m_factTrees.size() - 1]->setProperties(100, 100);
+	ptrToImpl->m_factLeaf[ptrToImpl->m_factLeaf.size() - 1]->setProperties(100, 100);
 	
 	SDL_Color white{ 255,255,255,255 };
 	
-	m_factFront.push_back(std::make_unique<FactoryOfFront>(renderer, white, "1 - Grass", 15, "E:/Arial.ttf"));
-	numbers.push_back(static_cast<int>(m_factFront.size() - 1));
+	ptrToImpl->m_factFront.push_back(std::make_unique<FactoryOfFront>(renderer, white, "1 - Grass", 15, "E:/Arial.ttf"));
+	ptrToImpl->numbers.push_back(static_cast<int>(ptrToImpl->m_factFront.size() - 1));
 
-	m_factFront.push_back(std::make_unique<FactoryOfFront>(renderer, white, "2 - Trees", 15, "E:/Arial.ttf"));
-	numbers.push_back(static_cast<int>(m_factFront.size() - 1));
+	ptrToImpl->m_factFront.push_back(std::make_unique<FactoryOfFront>(renderer, white, "2 - Trees", 15, "E:/Arial.ttf"));
+	ptrToImpl->numbers.push_back(static_cast<int>(ptrToImpl->m_factFront.size() - 1));
 
-	m_factFront.push_back(std::make_unique<FactoryOfFront>(renderer, white, "3 - Leaf", 15, "E:/Arial.ttf"));
-	numbers.push_back(static_cast<int>(m_factFront.size() - 1));
+	ptrToImpl->m_factFront.push_back(std::make_unique<FactoryOfFront>(renderer, white, "3 - Leaf", 15, "E:/Arial.ttf"));
+	ptrToImpl->numbers.push_back(static_cast<int>(ptrToImpl->m_factFront.size() - 1));
 
-	m_forChoose = 1;
+	ptrToImpl->m_forChoose = 1;
 }
 
 void Editor::deleteBlock(SDL_Renderer* renderer, App* app)
@@ -136,27 +63,20 @@ void Editor::deleteBlock(SDL_Renderer* renderer, App* app)
 	int mouseX = app->mouseX();
 	int mouseY = app->mouseY();
 	
-	auto deleteElMap = [&](std::multimap<std::string, int>& obj,int number) 
+	auto deleteElMap = [&](std::multimap<std::string, int>& obj,int number, std::string type) 
 	{
-		size_t number_ = 0;
-		for (auto it = obj.begin(); it != obj.end();++it) {
-			if (it->second == number) {
-				obj.erase(it);
-				break;
+			for (auto it = obj.begin(); it != obj.end(); ++it) {
+				if (it->first == type && it->second == number) {
+					it = obj.erase(it);
+					break;
+				}
 			}
-			number_++;
-		}
-		auto it = lastEl.begin();
-		for (size_t i = 0; i < lastEl.size(); ++i)
-		{
-			if (i >= number_) {
-				it->second--;
-				it++;
+			for (auto& [key,value] : obj)
+			{
+				if (key == type && value > number) value--;
 			}
-			else { ++it; }
-		}
 	};
-	auto deleteFromExact = [&](std::vector<std::unique_ptr<FactoryOfRect>>& obj)
+	auto deleteFromExact = [&](std::vector<std::unique_ptr<FactoryOfRect>>& obj,std::string type)
 		{
 			auto it = obj.begin();
 			for (size_t i = 0; i < obj.size(); i++)
@@ -164,27 +84,49 @@ void Editor::deleteBlock(SDL_Renderer* renderer, App* app)
 				if (obj[i]->mouseInRect(mouseX, mouseY))
 				{
 					it = obj.erase(it);
-					deleteElMap(lastEl, static_cast<int>(i));
+					deleteElMap(ptrToImpl->lastEl, static_cast<int>(i),type);
 					break;
 				}
 				else it++;
 			}
 		};
 
-	deleteFromExact(m_factGrass);
-	deleteFromExact(m_factLeaf);
-	deleteFromExact(m_factTrees);
+	deleteFromExact(ptrToImpl->m_factGrass,"A Grass");
+	deleteFromExact(ptrToImpl->m_factLeaf,"C Leaf");
+	deleteFromExact(ptrToImpl->m_factTrees,"B Trees");
 
-	for (auto& i : lastEl)
+	for (auto& i : ptrToImpl->lastEl)
 	{
 		std::cout << i.second << " : " << i.first << '\n';
 	}
 }
 
+//std::tuple<bool,
+//	std::vector<std::unique_ptr<FactoryOfRect>>::iterator,
+//	int> grass = deleteFromExact(m_factGrass);
+//std::tuple<bool,
+//	std::vector<std::unique_ptr<FactoryOfRect>>::iterator,
+//	int> leaf = deleteFromExact(m_factLeaf);
+//std::tuple<bool,
+//	std::vector<std::unique_ptr<FactoryOfRect>>::iterator,
+//	int> trees = deleteFromExact(m_factTrees);
+//
+//if (std::get<bool>(grass)) {
+//	m_factGrass.erase(std::get<std::vector<std::unique_ptr<FactoryOfRect>>::iterator>(grass));
+//	deleteElMap(lastEl, std::get<int>(grass));
+//}
+//else if (std::get<bool>(leaf)) {
+//	m_factLeaf.erase(std::get<std::vector<std::unique_ptr<FactoryOfRect>>::iterator>(leaf));
+//	deleteElMap(lastEl, std::get<int>(leaf));
+//}
+//else if (std::get<bool>(trees)) {
+//	m_factTrees.erase(std::get<std::vector<std::unique_ptr<FactoryOfRect>>::iterator>(trees));
+//	deleteElMap(lastEl, std::get<int>(trees));
+//}
 
-void Editor::forApp(int& number, SDL_Renderer* renderer, App* app)
+void Editor::HandleActions(int& number, SDL_Renderer* renderer, App* app)
 {
-	if (menuForEditor == 0) {
+	if (ptrToImpl->menuForEditor == 0) {
 		static int w = 100;
 		static int h = 100;
 
@@ -196,80 +138,82 @@ void Editor::forApp(int& number, SDL_Renderer* renderer, App* app)
 				obj[obj.size() - 1]->setProperties(w, h);
 			};
 		auto hideImpl = [&]() {
-			if (!m_factGrass.empty()) m_factGrass.pop_back();
-			if (!m_factLeaf.empty())  m_factLeaf.pop_back();
-			if (!m_factTrees.empty()) m_factTrees.pop_back();
+			if (!ptrToImpl->m_factGrass.empty()) ptrToImpl->m_factGrass.pop_back();
+			if (!ptrToImpl->m_factLeaf.empty())  ptrToImpl->m_factLeaf.pop_back();
+			if (!ptrToImpl->m_factTrees.empty()) ptrToImpl->m_factTrees.pop_back();
 			for (size_t i = 0; i < 3; ++i)
 			{
-				if (!m_factFront.empty()) m_factFront.pop_back();
+				if (!ptrToImpl->m_factFront.empty()) ptrToImpl->m_factFront.pop_back();
 			}
-			m_forChoose = 0;
-			size = 0;
-			number_b = 1;
+			ptrToImpl->m_forChoose = 0;
+			ptrToImpl->size = 0;
+			ptrToImpl->number_b = 1;
 			};
 		auto plusOrMinus = [&](SDL_Event event) 
 		{
+				auto r = ptrToImpl->rgbColors.find('r');
+				auto g = ptrToImpl->rgbColors.find('g');
+				auto b = ptrToImpl->rgbColors.find('b');
+				auto a = ptrToImpl->rgbColors.find('a');
+		
 				const Uint8* state = SDL_GetKeyboardState(NULL);
 				bool shift = state[SDL_SCANCODE_LSHIFT] || state[SDL_SCANCODE_RSHIFT];
-
-				auto r = rgbColors.find('r');
-				auto g = rgbColors.find('g');
-				auto b = rgbColors.find('b');
-				auto a = rgbColors.find('a');
-
-				if ((event.key.keysym.sym == SDLK_EQUALS || event.key.keysym.sym == SDLK_PLUS) && shift) {
-					if (state[SDL_SCANCODE_R]) {
-						std::cout << "R++\n";
+				bool plus = state[SDL_SCANCODE_EQUALS] || state[SDLK_PLUS];
+				bool minus = state[SDLK_UNDERSCORE] || state[SDL_SCANCODE_MINUS];
+				
+				if (plus) {
+					if (shift && state[SDL_SCANCODE_R]) {
+						std::cout << "r++ \n";
 						r->second++;
 					}
-					else if (state[SDL_SCANCODE_G]) {
-						std::cout << "G++\n";
+					else if (shift && state[SDL_SCANCODE_G]) {
+						std::cout << "g++ \n";
 						g->second++;
 					}
-					else if (state[SDL_SCANCODE_B]) {
-						std::cout << "B++\n";
+					else if (shift && state[SDL_SCANCODE_B]) {
+						std::cout << "b++ \n";
 						b->second++;
 					}
-					else if (state[SDL_SCANCODE_A]) {
-						std::cout << "A++\n";
+					else if (shift && state[SDL_SCANCODE_A]) {
+						std::cout << "a++ \n";
 						a->second++;
 					}
 				}
-
-				else if ((event.key.keysym.sym == SDLK_MINUS || event.key.keysym.sym == SDLK_UNDERSCORE) && shift) {
-					if (state[SDL_SCANCODE_R]) {
-						std::cout << "R--\n";
+				else if (minus) {
+					if (shift && state[SDL_SCANCODE_R]) {
+						std::cout << "r-- \n";
 						r->second--;
 					}
-					else if (state[SDL_SCANCODE_G]) {
-						std::cout << "G--\n";
+					else if (shift && state[SDL_SCANCODE_G]) {
+						std::cout << "g-- \n";
 						g->second--;
 					}
-					else if (state[SDL_SCANCODE_B]) {
-						std::cout << "B--\n";
+					else if (shift && state[SDL_SCANCODE_B]) {
+						std::cout << "b-- \n";
 						b->second--;
 					}
-					else if (state[SDL_SCANCODE_A]) {
-						std::cout << "A--\n";
+					else if (shift && state[SDL_SCANCODE_A]) {
+						std::cout << "a-- \n";
 						a->second--;
 					}
 				}
-
 		};
 		
 		SDL_Event event;
 		while (SDL_PollEvent(&event)) {
-			if (size == 1) currentTexture(m_factGrass);
-			else if (size == 3) currentTexture(m_factLeaf);
-			else if (size == 2) currentTexture(m_factTrees);
+			plusOrMinus(event);
 
-			if (m_forChoose == 1) {
-				m_factGrass[m_factGrass.size() - 1]->setPosition(app->mouseX(), app->mouseY());
-				m_factFront[numbers[0]]->textToRect(m_factGrass[m_factGrass.size() - 1]->getRect());
-				m_factLeaf[m_factLeaf.size() - 1]->setPosition(app->mouseX(), app->mouseY() - 200);
-				m_factFront[numbers[2]]->textToRect(m_factLeaf[m_factLeaf.size() - 1]->getRect());
-				m_factTrees[m_factTrees.size() - 1]->setPosition(app->mouseX(), app->mouseY() - 100);
-				m_factFront[numbers[1]]->textToRect(m_factTrees[m_factTrees.size() - 1]->getRect());
+			if (ptrToImpl->size == 1) currentTexture(ptrToImpl->m_factGrass);
+			else if (ptrToImpl->size == 3) currentTexture(ptrToImpl->m_factLeaf);
+			else if (ptrToImpl->size == 2) currentTexture(ptrToImpl->m_factTrees);
+
+			if (ptrToImpl->m_forChoose == 1) {
+				ptrToImpl->m_factGrass[ptrToImpl->m_factGrass.size() - 1]->setPosition(app->mouseX(), app->mouseY());
+				ptrToImpl->m_factFront[ptrToImpl->numbers[0]]->textToRect(ptrToImpl->m_factGrass[ptrToImpl->m_factGrass.size() - 1]->getRect());
+				ptrToImpl->m_factLeaf[ptrToImpl->m_factLeaf.size() - 1]->setPosition(app->mouseX(), app->mouseY() - 200);
+				ptrToImpl->m_factFront[ptrToImpl->numbers[2]]->textToRect(ptrToImpl->m_factLeaf[ptrToImpl->m_factLeaf.size() - 1]->getRect());
+				ptrToImpl->m_factTrees[ptrToImpl->m_factTrees.size() - 1]->setPosition(app->mouseX(), app->mouseY() - 100);
+				ptrToImpl->m_factFront[ptrToImpl->numbers[1]]->textToRect(ptrToImpl->m_factTrees[ptrToImpl->m_factTrees.size() - 1]->getRect());
 
 				if (event.type == SDL_KEYDOWN) {
 					if (event.key.keysym.sym == SDLK_1)
@@ -297,27 +241,27 @@ void Editor::forApp(int& number, SDL_Renderer* renderer, App* app)
 				if (event.wheel.y > 0) {
 					std::cout << "Mouse wheel up\n";
 					w++; h++;
-					if (size == 1) currentTexture(m_factGrass);
-					else if (size == 3) currentTexture(m_factLeaf);
-					else if (size == 2) currentTexture(m_factTrees);
+					if (ptrToImpl->size == 1) currentTexture(ptrToImpl->m_factGrass);
+					else if (ptrToImpl->size == 3) currentTexture(ptrToImpl->m_factLeaf);
+					else if (ptrToImpl->size == 2) currentTexture(ptrToImpl->m_factTrees);
 				}
 				if (event.wheel.y < 0) {
 					std::cout << "Mouse wheel down\n";
 					w--;h--;
-					if (size == 1) currentTexture(m_factGrass);
-					else if (size == 3) currentTexture(m_factLeaf);
-					else if (size == 2) currentTexture(m_factTrees);
+					if (ptrToImpl->size == 1) currentTexture(ptrToImpl->m_factGrass);
+					else if (ptrToImpl->size == 3) currentTexture(ptrToImpl->m_factLeaf);
+					else if (ptrToImpl->size == 2) currentTexture(ptrToImpl->m_factTrees);
 				}
 			}
 			else if (event.type == SDL_MOUSEBUTTONDOWN) {
 				if (event.button.button == SDL_BUTTON_LEFT) {
 					std::cout << "left\n";
-					if (size != 0) freezeBlock(renderer, app);
+					if (ptrToImpl->size != 0) freezeBlock(renderer, app);
 					else {
-						SDL_Rect rect = m_factEmpty[0]->getRect();
+						SDL_Rect rect = ptrToImpl->m_factEmpty[0]->getRect();
 						if (app->mouseX() > rect.x && app->mouseX() < (rect.x + rect.w)
 							&& app->mouseY() > rect.y && app->mouseY() < (rect.y + rect.h))
-							menuForEditor = 1;
+							ptrToImpl->menuForEditor = 1;
 					}
 				}
 				else if (event.button.button == SDL_BUTTON_RIGHT) {
@@ -326,7 +270,28 @@ void Editor::forApp(int& number, SDL_Renderer* renderer, App* app)
 				}
 				else if (event.button.button == SDL_BUTTON_MIDDLE) {
 					std::cout << "middle\n";
-					if (m_forChoose == 0) {
+					if (ptrToImpl->size != 0) {
+						auto detected = [&](std::string text) {
+							auto it = ptrToImpl->lastEl.begin();
+							for (auto iter = ptrToImpl->lastEl.begin(); iter != ptrToImpl->lastEl.end();++iter) if (iter->first == text) it = iter;
+							ptrToImpl->lastEl.erase(it);
+							};
+
+						if (ptrToImpl->size == 1) {
+							if (!ptrToImpl->m_factGrass.empty()) ptrToImpl->m_factGrass.pop_back();
+							detected("A Grass");
+						}
+						else if (ptrToImpl->size == 2) {
+							if (!ptrToImpl->m_factTrees.empty()) ptrToImpl->m_factTrees.pop_back();
+							detected("B Trees");
+						}
+						else if (ptrToImpl->size == 3) {
+							if (!ptrToImpl->m_factLeaf.empty()) ptrToImpl->m_factLeaf.pop_back();
+							detected("C Leaf");
+						}
+						ptrToImpl->size = 0;
+					}
+					if (ptrToImpl->m_forChoose == 0) {
 						chooseBlock(renderer);
 					}
 					else {
@@ -343,21 +308,12 @@ void Editor::forApp(int& number, SDL_Renderer* renderer, App* app)
 					std::cout << "clearAll\n";
 					clearAll(renderer);
 				}
-				else if (event.key.keysym.sym == SDLK_r) {
-					std::cout << "R\n";
-					plusOrMinus(event);
-				}
-				else if (event.key.keysym.sym == SDLK_g) {
-					std::cout << "G\n";
-					plusOrMinus(event);
-				}
-				else if (event.key.keysym.sym == SDLK_b) {
-					std::cout << "B\n";
-					plusOrMinus(event);
-				}
-				else if (event.key.keysym.sym == SDLK_a) {
-					std::cout << "A\n";
-					plusOrMinus(event);
+				else if (event.key.keysym.sym == SDLK_z) {
+					std::cout << "z\n";
+					auto r = ptrToImpl->rgbColors.find('r'); r->second = 0;
+					auto g = ptrToImpl->rgbColors.find('g'); g->second = 0;
+					auto b = ptrToImpl->rgbColors.find('b'); b->second = 0;
+					auto a = ptrToImpl->rgbColors.find('a'); a->second = 0;
 				}
 			}
 		}
@@ -370,7 +326,7 @@ void Editor::forApp(int& number, SDL_Renderer* renderer, App* app)
 			}
 			if (event.key.keysym.sym == SDLK_ESCAPE) {
 				std::cout << "escape\n";
-				menuForEditor = 0;
+				ptrToImpl->menuForEditor = 0;
 			}
 		}
 	}
@@ -379,19 +335,19 @@ void Editor::forApp(int& number, SDL_Renderer* renderer, App* app)
 void Editor::keeperBlock(int number, SDL_Renderer* renderer)
 {
 	if (number == 1) {
-		size = 1;
-		m_factGrass.push_back(std::make_unique<FactoryOfRect>(renderer, "E:/grass.png"));
+		ptrToImpl->size = 1;
+		ptrToImpl->m_factGrass.push_back(std::make_unique<FactoryOfRect>(renderer, "E:/grass.png"));
 	}
 	else if (number == 3) {
-		size = 3;
-		m_factLeaf.push_back(std::make_unique<FactoryOfRect>(renderer, "E:/leaf.png"));
+		ptrToImpl->size = 3;
+		ptrToImpl->m_factLeaf.push_back(std::make_unique<FactoryOfRect>(renderer, "E:/leaf.png"));
 	}
 	else if (number == 2) {
-		size = 2;
-		m_factTrees.push_back(std::make_unique<FactoryOfRect>(renderer, "E:/wood.png"));
+		ptrToImpl->size = 2;
+		ptrToImpl->m_factTrees.push_back(std::make_unique<FactoryOfRect>(renderer, "E:/wood.png"));
 	}
 	else {
-		size = 0;
+		ptrToImpl->size = 0;
 		std::cout << "Nothing is gonna happen!\n";
 	}
 }
