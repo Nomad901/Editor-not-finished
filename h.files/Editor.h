7eab9311e9adc09d18c5de21c5f7bf024bdad449
Tango <maxim.ant07@gmail.com>
2025-04-11 18:54:17 +0300

    Update Editor.h

--- h.files/Editor.h
+++ h.files/Editor.h
@@ -4,6 +4,10 @@
 #include <map>
 #include <string>
 #include <memory>
+#include <unordered_map>
+#include <thread>
+#include <future>
+#include <tuple>
 #include <SDL.h>
 #include <SDL_image.h>
 #include "FactoryOfRect.h"
@@ -16,37 +20,51 @@ import MenuForEditor;
 class Editor
 {
 public:
+	Editor() = default;
 	Editor(App* app, SDL_Renderer* renderer);
 	~Editor() = default;
 
 	void freezeBlock(SDL_Renderer* renderer, App* app);
 	void chooseBlock(SDL_Renderer* renderer);
 	void deleteBlock(SDL_Renderer* renderer, App* app);
-	void clearAll(SDL_Renderer* renderer) { lastEl.clear(); }
-	
-	void render(SDL_Renderer* renderer);
-	
-	void forApp(int& number, SDL_Renderer* renderer, App* app);
+	void clearAll(SDL_Renderer* renderer) { ptrToImpl->lastEl.clear(); }
 
-	//bool getChoose() { return m_forChoose; }
-	//std::vector<std::unique_ptr<FactoryOfRect>> getFactGrass() { return m_factGrass; }
-	//std::vector<std::unique_ptr<FactoryOfRect>> getFactTrees() { return m_factTrees; }
-	//std::vector<std::unique_ptr<FactoryOfRect>> getFactLeaf() { return m_factLeaf; }
-	//std::vector<std::unique_ptr<FactoryOfFront>> getFactFront() { return m_factFront; }
+	//void render(SDL_Renderer* renderer);
+	//void renderBackground(SDL_Renderer* renderer);
+
+	void HandleActions(int& number, SDL_Renderer* renderer, App* app);
 
 private:
 	void keeperBlock(int number, SDL_Renderer* renderer);
 
-	int size;
-	bool number_b;
-	bool m_forChoose; 
-	bool menuForEditor;
-	std::multimap<std::string, int> lastEl;
-	std::vector<int> numbers;
-	std::vector<std::unique_ptr<FactoryOfRect>> m_factGrass;
-	std::vector<std::unique_ptr<FactoryOfRect>> m_factTrees;
-	std::vector<std::unique_ptr<FactoryOfRect>> m_factLeaf;
-	std::vector<std::unique_ptr<FactoryOfRect>> m_factEmpty;
-	std::vector<std::unique_ptr<FactoryOfFront>> m_factFront;
-	MenuForEditor menu;
+	struct impl {
+		impl() {
+			size = 0;
+			number_b = 1;
+			m_forChoose= 0;
+			menuForEditor = 0;
+		}
+		
+		int size;
+		bool number_b;
+		bool m_forChoose;
+		bool menuForEditor;
+		bool blockFrozen;
+		std::unordered_map<char, uint8_t> rgbColors{ {'r',0},{'g',0},{'b',0},{'a',0} };
+		std::multimap<std::string, int> lastEl;
+		std::vector<int> numbers;
+		std::vector<std::unique_ptr<FactoryOfRect>> m_factGrass;
+		std::vector<std::unique_ptr<FactoryOfRect>> m_factTrees;
+		std::vector<std::unique_ptr<FactoryOfRect>> m_factLeaf;
+		std::vector<std::unique_ptr<FactoryOfRect>> m_factEmpty;
+		std::vector<std::unique_ptr<FactoryOfFront>> m_factFront;
+	};
+	std::unique_ptr<impl> ptrToImpl;
+	std::unique_ptr<MenuForEditor> menu;
+
+	static std::unique_ptr<impl> ptrToImplForRenderer;
+	static std::unique_ptr<MenuForEditor> menuForRenderer;
+
+	friend class Renderer;
 };
+
