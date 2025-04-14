2bfae67693d5ee82f73eac4d1a2685b902607056
Tango <maxim.ant07@gmail.com>
2025-04-14 20:43:03 +0300

    Update Menu.cpp

--- cpp.files/Menu.cpp
+++ cpp.files/Menu.cpp
@@ -1,8 +1,10 @@
 #include "Menu.h"
 
-Menu::Menu()
-	:mouseX(0), mouseY(0) 
-{}
+Menu::Menu() 
+{
+	pImpl = std::make_unique<impl>();
+	pImplForRenderer = pImpl.get();
+}
 
 Menu& Menu::getInstance()
 {
@@ -10,47 +12,42 @@ Menu& Menu::getInstance()
 	return instance;
 }
 
-void Menu::background(SDL_Window* window, SDL_Renderer* renderer)
-{
-	int w, h;
-	SDL_GetWindowSize(window, &w, &h);
-	FactoryOfRect f(renderer, "E:/output-onlinepngtools.png");
-	f.setProperties(w, h);
-	f.render(renderer);
-}
+void Menu::background(SDL_Window* window, SDL_Renderer* renderer, std::string backgroundPath){
+		int w, h;
+		SDL_GetWindowSize(window, &w, &h);
+		FactoryOfRect f(renderer, backgroundPath.c_str());
+		f.setProperties(w, h);
+		f.render(renderer);
+};
+
 
 int Menu::buttons(SDL_Renderer* renderer)
 {
 	if (TTF_Init() == -1)
 		std::cout << "Couldnt initiazlize TTF! " << TTF_GetError() << '\n';
 	SDL_Color white = { 255, 255, 255, 255 };
-	m_textVct.push_back(std::make_unique<FactoryOfFront>(renderer, white, "Play", 24, "E:/Arial.ttf"));
-	m_textVct.push_back(std::make_unique<FactoryOfFront>(renderer, white, "Editor", 24, "E:/Arial.ttf"));
-	m_textVct.push_back(std::make_unique<FactoryOfFront>(renderer, white, "Settings", 24, "E:/Arial.ttf"));
-	m_textVct.push_back(std::make_unique<FactoryOfFront>(renderer, white, "Exit", 24, "E:/Arial.ttf"));
+	pImpl->m_textVct.push_back(std::make_unique<FactoryOfFront>(renderer, white, "Play", 24, "E:/Arial.ttf"));
+	pImpl->m_textVct.push_back(std::make_unique<FactoryOfFront>(renderer, white, "Editor", 24, "E:/Arial.ttf"));
+	pImpl->m_textVct.push_back(std::make_unique<FactoryOfFront>(renderer, white, "Settings", 24, "E:/Arial.ttf"));
+	pImpl->m_textVct.push_back(std::make_unique<FactoryOfFront>(renderer, white, "Exit", 24, "E:/Arial.ttf"));
 
 	const int x = 500;
 	int y = 100;
 	for (int i = 0; i < 4; i++)
 	{
-		m_smrtVct.push_back(std::make_unique<FactoryOfRect>(renderer, "E:/grass.png"));
+		pImpl->m_smrtVct.push_back(std::make_unique<FactoryOfRect>(renderer, "E:/grass.png"));
 	}
-	for (size_t i = 0; i < m_smrtVct.size(); i++)
+	for (size_t i = 0; i < pImpl->m_smrtVct.size(); i++)
 	{
-		m_smrtVct[i]->setProperties(300, 100);
-		m_smrtVct[i]->setPosition(x, y);
+		pImpl->m_smrtVct[i]->setProperties(300, 100);
+		pImpl->m_smrtVct[i]->setPosition(x, y);
 		y += 100;
 	}
-	for (size_t i = 0; i < 4; i++)
-	{
-		m_textVct[i]->textToRect(m_smrtVct[i]->getRect());
-		m_textVct[i]->render(renderer);
-	}
-	
-	SDL_Rect rect1 = m_smrtVct[0]->getRect();
-	SDL_Rect rect2 = m_smrtVct[1]->getRect();
-	SDL_Rect rect3 = m_smrtVct[2]->getRect();
-	SDL_Rect rect4 = m_smrtVct[3]->getRect();
+
+	SDL_Rect rect1 = pImpl->m_smrtVct[0]->getRect();
+	SDL_Rect rect2 = pImpl->m_smrtVct[1]->getRect();
+	SDL_Rect rect3 = pImpl->m_smrtVct[2]->getRect();
+	SDL_Rect rect4 = pImpl->m_smrtVct[3]->getRect();
 
 	SDL_Event event;
 	while (SDL_PollEvent(&event)) {
@@ -76,7 +73,5 @@ int Menu::buttons(SDL_Renderer* renderer)
 				return 4;
 		}
 	}
-
-	return 0;
-	
+	return 0;	
 }
